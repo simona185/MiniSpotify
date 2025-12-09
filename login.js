f7870903aaea99b0b840990c35e2cd2549455870
simona185 <simonaparaschiv185@gmail.com>
2025-12-09 22:05:33 +0200

    Update login.js

--- login.js
+++ login.js
@@ -5,4 +5,7 @@ document.getElementById("login").onclick = () => {
   const scope = "user-read-private user-top-read";
   const auth_url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${client_id}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
   window.location = auth_url;
+
+  console.log("login.js loaded!");
+
 };
